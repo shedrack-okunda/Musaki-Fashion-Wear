bf9c0a9cef6335b46160d1bec220f239a4d63a3e
Sheddy <okundashedrack@gmail.com>
2025-06-24 21:40:54 +0300

    deleted the models

--- backend/models/Brand.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const { Schema } = mongoose;
-
-const brandSchema = new Schema({
-  name: { type: String, required: true },
-});
-
-const Brand = mongoose.model("Brand", brandSchema);
-export default Brand;
--- backend/models/Category.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const { Schema } = mongoose;
-
-const categorySchema = new Schema({
-  name: { type: String, required: true },
-});
-
-const Category = mongoose.model("Category", categorySchema);
-export default Category;
